22959c293731bdd19efc9970d803a5a837e85ccd
YeEun <kye6735@naver.com>
2024-03-14 21:53:47 +0900

    [PCCE 기출문제] 1번/출력

---
+++ programmers/Level.0/PCCE기출문제_출력.java
@@ -0,0 +1,15 @@
+/*주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다.
+아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.*/
+
+public class PCCE기출문제_출력 {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
+
