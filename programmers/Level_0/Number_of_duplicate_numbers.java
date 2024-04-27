053de287b9bdd1baf3a81480d8afd46d607dc307
y2eun <kye6735@naver.com>
2024-04-27 10:53:44 +0900

    중복된 숫자 개수 (파일 이동)

--- programmers/Level_0/Number_of_duplicate_numbers.java
+++ programmers/Level_0/Number_of_duplicate_numbers.java
@@ -1,5 +1,7 @@
 /* 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요. */
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int [] array, int n) {
         int answer = 0;
@@ -17,10 +19,10 @@ public class Number_of_duplicate_numbers {
         int[] array = {1, 1, 2, 3, 4, 5};
         int n = 1;
         int result = sol.solution(array, n);
-        System.out.println("result = " + result);
+        System.out.println("array = " + Arrays.toString(array) + " | result = " + result);
 
         array = new int[]{0, 2, 3, 4};
         result = sol.solution(array, n);
-        System.out.println("result = " + result);
+        System.out.println("array = " + Arrays.toString(array) + " | result = " + result);
     }
 }
