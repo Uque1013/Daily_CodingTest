00c82aae6c421a0872a787fe61cced443c0112b1
y2eun <kye6735@naver.com>
2024-04-27 11:09:05 +0900

    치치보다 키 큰 사람 (파일 이동)

--- programmers/Level_0/Who_is_taller_than_Chichi.java
+++ programmers/Level_0/Who_is_taller_than_Chichi.java
@@ -2,6 +2,8 @@
 치치네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때,
 치치보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요. */
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
@@ -22,12 +24,14 @@ public class Who_is_taller_than_Chichi {
         int height = 167;
         int result = sol.solution(array, height);
 
-        System.out.println("result = " + result);
+        System.out.println("치치의 키 : " + height);
+        System.out.println(Arrays.toString(array) +" | result = " + result);
 
         array = new int[] {180, 120, 140};
         height = 190;
         result = sol.solution(array, height);
 
-        System.out.println("result = " + result);
+        System.out.println("치치의 키 : " + height);
+        System.out.println(Arrays.toString(array) +" | result = " + result);
     }
 }
